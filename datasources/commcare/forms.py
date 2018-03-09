082318df95e1a7fe750ad475045bae7884191cb4
Sanjuro Jogdeo <sjogdeo@mercycorps.org>
2018-03-09 13:32:59 -0800

    Restored API Key form and started turning into a unified form that can handle both API Key and Password

--- datasources/commcare/forms.py
+++ datasources/commcare/forms.py
@@ -2,7 +2,7 @@ from django.core.urlresolvers import reverse_lazy
 from django.forms import ModelForm
 from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Div, Submit, Reset, HTML, Button, Row, Field, Hidden
+from crispy_forms.layout import Layout, Div, Submit, Reset, HTML, Button, Row, Field, Hidden, Fieldset
 from crispy_forms.bootstrap import FormActions
 from django.forms.formsets import formset_factory
 
@@ -34,6 +34,7 @@ class CommCarePassForm(forms.Form):
     username = forms.CharField(max_length=60, required=True)
     password = forms.CharField(required=True, widget=forms.PasswordInput())
     project = forms.CharField(required=True, help_text=mark_safe("This is the name of the project you are importing from. Press the down arrow to see the name of past projects you have imported from. To see the name of your CommCare projects go to CommCare <a href='https://www.commcarehq.org/account/projects/#'>settings then click my projects</a>"))
+    download_type = forms.CharField(required=True, widget=ListTextWidget(data_list=['Forms', 'Cases'], name='download_type'))
     silo = forms.ChoiceField(required=True)
 
 
@@ -57,6 +58,9 @@ class CommCareProjectForm(forms.Form):
     project = forms.CharField(required=True, help_text=mark_safe("This is the name of the project you are importing from. Press the down arrow to see the name of past projects you have imported from. The projects your account has access to are listed in your CommCare <a href='https://www.commcarehq.org/account/projects/' target='_blank'>settings</a> under my projects.<br/>If you are not getting access it could be because your project has a different name then what you as a user can see. To see your projects true name go to CommCare <a href='https://www.commcarehq.org/account/projects/' target='_blank'>settings</a>"))
     silo = forms.ChoiceField(required=True)
 
+    TYPE_CHOICES = [('forms', 'Forms'), ('cases', 'Cases')]
+    download_type = forms.ChoiceField(choices=TYPE_CHOICES, widget=forms.RadioSelect())
+    form_name = forms.CharField(required=False)
 
     def __init__(self, *args, **kwargs):
         choices = kwargs.pop('choices')
@@ -78,12 +82,26 @@ class CommCareProjectForm(forms.Form):
 class CommCareAuthForm(CommCareProjectForm):
     username = forms.CharField(max_length=60, required=True)
     auth_token = forms.CharField(required=True, widget=forms.PasswordInput(), help_text=mark_safe("This gives tola access to your CommCare reports. Your api key can be found in your CommCare <a href='https://www.commcarehq.org/account/settings/' target='_blank'>settings</a>"))
+    password = forms.CharField(required=True, widget=forms.PasswordInput())
 
     def __init__(self, *args, **kwargs):
         super(CommCareAuthForm, self).__init__(*args, **kwargs)
-        self.fields.keyOrder = [
-            'username',
-            'auth_token',
-            'project',
-            'silo']
         self.fields['auth_token'].label = 'API Key'
+        self.helper.layout = Layout(
+            Fieldset(
+                'Login Information',
+                'username',
+                Div(
+                    'auth_token',
+                    'password',
+                    css_id = 'div_id_credentials'
+                )
+            ),
+            Fieldset(
+                'Download Details',
+                'project',
+                'silo',
+                'download_type',
+                'form_name',
+            )
+        )
--- datasources/commcare/urls.py
+++ datasources/commcare/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     # url(r'^$', views.getCommCareAuth, name='getCommCareAuth'),
-    # url(r'^passform/', views.getCommCareFormPass, name='getCommCarePass'),
-    url(r'^$', views.getCommCareFormPass, name='getCommCarePass'),
+    url(r'^passform/', views.getCommCareFormPass, name='getCommCarePass'),
+    url(r'^$', views.getCommCareAuth, name='getCommCareAuth'),
     url(r'^logout/$',views.commcareLogout, name='commcareLogout'),
 ]
