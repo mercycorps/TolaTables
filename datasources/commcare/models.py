5742357d4834deaafaa9e2ff7392ce5d1b255119
Sanjuro Jogdeo <sjogdeo@mercycorps.org>
2018-04-04 10:36:12 -0700

    Create model to track CommCare Cache

--- datasources/commcare/models.py
+++ datasources/commcare/models.py
@@ -2,5 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from silo.models import Silo
 
 # Create your models here.
+
+class CommCareCache(models.Model):
+    project = models.CharField(max_length=255)
+    form_name = models.CharField(max_length=1000)
+    form_id = models.CharField(max_length=255)
+    silo = models.OneToOneField(Silo)
+    last_updated = models.DateTimeField()
